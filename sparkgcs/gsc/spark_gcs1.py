c867c7f275f97cad2ef303716d1527cd32895ed3
vicky <vicky@email.com>
2023-09-02 22:28:46 +0530

    commit

--- sparkgcs/gsc/spark_gcs1.py
+++ sparkgcs/gsc/spark_gcs1.py
@@ -22,21 +22,24 @@ def main():
     conf.set("google.cloud.auth.service.account.json.keyfile","/home/hduser/install/projectbigdata-395203-26f51bbb59ce.json")
     print("Data Transfer between HDFS to GCS and Vice versa")
 # file path and file names
-    hdfs_path=""
+    hdfs_path="hdfs://localhost:54310/user/hduser/datatotransfer/"
 
 #getting current date
     curr_date =date.today().strftime("%m%d%y")
-    file_name =  "custs_"+curr_date
+    file_name = "custs_"+curr_date
     gcs_path = "gs://bigdatavicky/"+file_name
 
 #service account to access gc with the private key
     path_to_private_key="/home/hduser/install/projectbigdata-395203-26f51bbb59ce.json"
+#check the file existence in the google cloud storage using google cloud storage api
     client = storage.Client.from_service_account_json(json_credentials_path=path_to_private_key)
     bucket = storage.Bucket(client,'bigdatavicky')
     blob = bucket.blob(file_name)
     if blob.exists():
         gcs_df = spark.read.option("header", "false").option("delimiter", ",").csv(gcs_path)
         gcs_df.show(2)
+        gcs_df.coalesce(1).write.csv(hdfs_path+file_name)
+        print(f'Todays dated file {file_name} has been transferred to HDFS Successfully')
     else:
         print("File is not available")
 
