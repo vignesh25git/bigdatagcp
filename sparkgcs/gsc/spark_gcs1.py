940defda7ce0721138c46964e031d26750c4b38e
vicky <vicky@email.com>
2023-09-02 22:09:56 +0530

    third commit

--- sparkgcs/gsc/spark_gcs1.py
+++ sparkgcs/gsc/spark_gcs1.py
@@ -1,9 +1,13 @@
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from pyspark.sql import SparkSession
+from datetime import date
+from google.cloud import storage
+
+import os
 
 def main():
-# define spark configuration object
+# create spark session object
     spark = SparkSession.builder\
                         .appName("HDFS - GCP GCS Read/Write") \
                         .config("spark.jars","/home/hduser/install/gcs-connector-hadoop2-2.2.7.jar")\
@@ -11,25 +15,29 @@ def main():
                         .getOrCreate()
     spark.sparkContext.setLogLevel("ERROR")
     sc=spark.sparkContext
+# spark configuration
     conf = spark.sparkContext._jsc.hadoopConfiguration()
     conf.set("fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem")
     conf.set("fs.AbstractFileSystem.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS")
     conf.set("google.cloud.auth.service.account.json.keyfile","/home/hduser/install/projectbigdata-395203-26f51bbb59ce.json")
     print("Data Transfer between HDFS to GCS and Vice versa")
-    hdfs_df=spark.read.csv("hdfs://localhost:54310/user/hduser/datatotransfer/")
-    print("HDFS Read Completed Successfully")
-    hdfscnt=hdfs_df.count()
-    curts = spark.createDataFrame([1], IntegerType()).withColumn("curts", current_timestamp()).select(date_format(col("curts"),
-    "yyyyMMddHHmmSS")).first()[0]
-    print(curts)
-    hdfs_df.coalesce(1).write.csv("gs://bigdatavicky/custdata_"+curts)
-    gcs_df = spark.read.option("header", "false").option("delimiter", ",")\
-                .csv("gs://bigdatavicky/custdata_"+curts)
-    gcs_df.show(2)
-    gcscnt=gcs_df.count()
-    if (hdfscnt==gcscnt):
-        print("gcs Write Completed Successfully including Data Quality/Reconcilation check completed (equivalent to sqoop --validate)")
+# file path and file names
+    hdfs_path=""
+
+#getting current date
+    curr_date =date.today().strftime("%m%d%y")
+    file_name =  "custs_"+curr_date
+    gcs_path = "gs://bigdatavicky/"+file_name
+
+#service account to access gc with the private key
+    path_to_private_key="/home/hduser/install/projectbigdata-395203-26f51bbb59ce.json"
+    client = storage.Client.from_service_account_json(json_credentials_path=path_to_private_key)
+    bucket = storage.Bucket(client,'bigdatavicky')
+    blob = bucket.blob(file_name)
+    if blob.exists():
+        gcs_df = spark.read.option("header", "false").option("delimiter", ",").csv(gcs_path)
+        gcs_df.show(2)
     else:
-        print("gcs Write Issue")
+        print("File is not available")
 
 main()
---
+++ sparkgcs/utils/getcurrdate.py
@@ -0,0 +1,7 @@
+from datetime import date
+
+today =date.today()
+
+curr_date = date.today().strftime("%m%d%y")
+
+print(curr_date)
